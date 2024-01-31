3c2404cd523537bf5fdbcfa5bdd9ca2f6cbbd09f
Sümeyye <52049640+smyyyaydn@users.noreply.github.com>
2024-01-31 16:02:08 +0300

    JWS
    token

---
+++ token/WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Security;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Token token = TokenHandler.CreateToken(_configuration);
+            return Ok(token);
+        }
+    }
+}
